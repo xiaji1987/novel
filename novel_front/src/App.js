8a4b6779f9755e8b0a4a667bb128160bd8309794
xiaji1987 <3094889344@qq.com>
2020-05-28 22:06:46 +0800

    小说阅读器

---
+++ novel_front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss'
+// import { BrowserRouter, Route } from 'react-router-dom'
+// import Initial from './pages/Initial'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ novel_front/src/pages/Initial.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Initial extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <>初始化页面</>
+    );
+  }
+}
+ 
+export default Initial;
